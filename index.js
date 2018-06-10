58b11b3ffb8039ac4b294f12f2cd8b60fd6c3086
Eddie C <eddiec76@gmail.com>
2018-06-10 20:36:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var recipes = new Object();
 
 function updateObjectWithKeyAndValue(object,key,value) {
-  object[key] = value;
-  return object;
+ 
 }
+
+/* object[key] = value;
+  return object;*/
